33ea7203a4da0a5e46381fc390a4786ecf521bf5
Yoshi Yamaguchi <ymotongpoo AT gmail.com>
2014-01-25 20:34:51 +0900

    first commit

---
+++ operate.go
@@ -0,0 +1,66 @@
+package bitarray
+
+import (
+	_ "bytes"
+	"errors"
+	"io"
+)
+
+const (
+	Uint8Size  = uint8(8)
+	Uint16Size = uint8(16)
+	Uint32Size = uint8(32)
+	Uint64Size = uint8(64)
+	Int8Size   = uint8(8)
+	Int16Size  = uint8(16)
+	Int32Size  = uint8(32)
+	Int64Size  = uint8(64)
+)
+
+var ErrSizeTooLarge = errors.New("bitarray: Specified is too large.")
+
+type Buffer struct {
+	buf   io.ByteReader
+	n     uint8 // Index of current bit position in byte segmentation.
+	extra uint8 // Extra byte unmanupilated in last operation.
+}
+
+func NewBuffer(b io.ByteReader) *Buffer {
+	return &Buffer{
+		buf: b,
+	}
+}
+
+func (b *Buffer) PopUint8(size uint8) (uint8, error) {
+	if size > Uint8Size {
+		return 0, ErrSizeTooLarge
+	}
+
+	var bin uint8
+	if b.n == 0 {
+		c, err := b.buf.ReadByte()
+		if err != nil {
+			return 0, err
+		}
+		bin = uint8(c) >> (Uint8Size - size)
+		b.n += size
+		b.extra = uint8(c) << size
+	} else {
+		if b.n+size < Uint8Size {
+			bin = b.extra >> (Uint8Size - size)
+			b.n += size
+			b.extra = b.extra << size
+		} else {
+			n := (b.n + size) % Uint8Size
+			c, err := b.buf.ReadByte()
+			if err != nil {
+				return 0, err
+			}
+			bin = uint8(c) >> (Uint8Size - n)
+			bin += b.extra >> (size - n)
+			b.extra = uint8(c) << n
+			b.n = n
+		}
+	}
+	return bin, nil
+}
---
+++ operate_test.go
@@ -0,0 +1,134 @@
+package bitarray
+
+import (
+	"bytes"
+	"reflect"
+	"testing"
+)
+
+func Setup() []*Buffer {
+	ins := [][]byte{
+		[]byte{
+			0x00, // 00000000
+			0xff, // 11111111
+		},
+		[]byte{
+			0xf0, // 11110000
+			0x0f, // 00001111
+		},
+		[]byte{
+			0xaa, // 10101010
+			0x55, // 01010101
+		},
+	}
+
+	bufs := make([]*Buffer, len(ins))
+	for i, bs := range ins {
+		buf := bytes.NewBuffer(bs)
+		bufs[i] = NewBuffer(buf)
+	}
+	return bufs
+}
+
+func TestPopUint8(t *testing.T) {
+	size := uint8(3)
+	ins := Setup()
+
+	// Phase 1) Fetch first 3 bits from elements in `ins`
+	uint8Wants := []uint8{
+		0x00, // -----|000
+		0x07, // -----|111
+		0x05, // -----|101
+	}
+	nWants := []uint8{3, 3, 3}
+	extraWants := []uint8{
+		0x00, // 00000|---
+		0x80, // 10000|---
+		0x50, // 01010|---
+	}
+	uint8Outs := make([]uint8, len(ins))
+	for i, c := range ins {
+		out, err := c.PopUint8(size)
+		if err != nil {
+			t.Error(err)
+		}
+		uint8Outs[i] = out
+	}
+	if !reflect.DeepEqual(uint8Wants, uint8Outs) {
+		t.Errorf("wants: %v, outs=%v", uint8Wants, uint8Outs)
+	}
+	for i, buf := range ins {
+		if buf.n != nWants[i] {
+			t.Errorf("%dth element: want: %v, out=%v", i, nWants[i], buf.n)
+		}
+		if buf.extra != extraWants[i] {
+			t.Errorf("%dth element: want: %v, out=%v", i, extraWants[i], buf.extra)
+		}
+	}
+
+	size2 := uint8(2)
+	// Phase 2) Fetch next 2 bits from elements in `ins`
+	uint8Wants2 := []uint8{
+		0x00, // ------|00
+		0x02, // ------|10
+		0x01, // ------|01
+	}
+	nWants2 := []uint8{5, 5, 5}
+	extraWants2 := []uint8{
+		0x00, // 000|-----
+		0x00, // 000|-----
+		0x40, // 010|-----
+	}
+	uint8Outs2 := make([]uint8, len(ins))
+	for i, c := range ins {
+		out, err := c.PopUint8(size2)
+		if err != nil {
+			t.Error(err)
+		}
+		uint8Outs2[i] = out
+	}
+	if !reflect.DeepEqual(uint8Wants2, uint8Outs2) {
+		t.Errorf("wants: %v, outs=%v", uint8Wants2, uint8Outs2)
+	}
+	for i, buf := range ins {
+		if buf.n != nWants2[i] {
+			t.Errorf("%dth element: want: %v, out=%v", i, nWants2[i], buf.n)
+		}
+		if buf.extra != extraWants2[i] {
+			t.Errorf("%dth element: want: %v, out=%v", i, extraWants2[i], buf.extra)
+		}
+	}
+
+	size3 := uint8(5)
+	// Phase 3) Fetch next 2 bits from elements in `ins`
+	uint8Wants3 := []uint8{
+		0x03, // ---|000,11
+		0x00, // ---|000,00
+		0x09, // ---|010,01
+	}
+	nWants3 := []uint8{2, 2, 2}
+	extraWants3 := []uint8{
+		0xfc, // 111111|--
+		0x3c, // 001111|--
+		0x54, // 010101|--
+	}
+	uint8Outs3 := make([]uint8, len(ins))
+	for i, c := range ins {
+		out, err := c.PopUint8(size3)
+		if err != nil {
+			t.Error(err)
+		}
+		uint8Outs3[i] = out
+	}
+	if !reflect.DeepEqual(uint8Wants3, uint8Outs3) {
+		t.Errorf("wants: %v, outs=%v", uint8Wants3, uint8Outs3)
+	}
+	for i, buf := range ins {
+		if buf.n != nWants3[i] {
+			t.Errorf("%dth element: want: %v, out=%v", i, nWants3[i], buf.n)
+		}
+		if buf.extra != extraWants3[i] {
+			t.Errorf("%dth element: want: %v, out=%v", i, extraWants3[i], buf.extra)
+		}
+	}
+}
