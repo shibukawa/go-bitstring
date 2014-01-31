419d75d3310b2e2a7b7d1447e82b4eed04f031d8
Yoshi Yamaguchi <ymotongpoo AT gmail.com>
2014-02-01 00:09:26 +0900

    release first implementation of decoder. passed test.

---
+++ decode.go
@@ -0,0 +1,132 @@
+/*
+Copyright 2014 Google Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package bitarray
+
+import (
+	"errors"
+	"io"
+	"log"
+	"reflect"
+	"strconv"
+)
+
+var (
+	ErrFieldSizeTooLarge = errors.New("bitarray: Specified bit size is too large for field")
+)
+
+// Decoder reads and decodes bit array objects from an input stream
+type Decoder struct {
+	buf *Buffer
+}
+
+// NewDecoder returns a new Decoder that reads from b.
+func NewDecoder(b *Buffer) *Decoder {
+	return &Decoder{
+		buf: b,
+	}
+}
+
+func Unmarshal(b *Buffer, v interface{}) error {
+	d := NewDecoder(b)
+	return d.Unmarshal(v)
+}
+
+func (d *Decoder) Unmarshal(v interface{}) error {
+	kind := reflect.ValueOf(v).Kind()
+	if kind != reflect.Ptr {
+		return errors.New("bitarray.Unmarshal: invalid type " + kind.String())
+	}
+
+	st := reflect.ValueOf(v).Elem()
+	typ := st.Type()
+	if typ.Kind() != reflect.Struct {
+		return errors.New("bitarray.Unmarshal: invalid type " + st.String())
+
+	}
+
+	for i := 0; i < st.NumField(); i++ {
+		field := st.Field(i)
+		sizeStr := typ.Field(i).Tag.Get("bit")
+		if len(sizeStr) == 0 {
+			continue
+		}
+		size, err := strconv.ParseUint(sizeStr, 0, 64)
+		if err != nil {
+			return err
+		}
+
+		// TODO(ymotongpoo): Require refactoring
+		switch field.Kind() {
+		case reflect.Uint8:
+			log.Printf("%s: Uint8", typ.Field(i).Name)
+			if size > Uint8Size {
+				return ErrFieldSizeTooLarge
+			}
+			bit, err := d.buf.PopUint8(size)
+			if err != nil && err != io.EOF {
+				return err
+			}
+			if typ.Field(i).Name == "_" {
+				continue
+			}
+			st.Field(i).SetUint(uint64(bit))
+		case reflect.Uint16:
+			log.Printf("%s: Uint16", typ.Field(i).Name)
+			if size > Uint16Size {
+				return ErrFieldSizeTooLarge
+			}
+			bit, err := d.buf.PopUint16(size)
+			if err != nil && err != io.EOF {
+				return err
+			}
+			if typ.Field(i).Name == "_" {
+				continue
+			}
+			st.Field(i).SetUint(uint64(bit))
+		case reflect.Uint32:
+			log.Printf("%s: Uint32", typ.Field(i).Name)
+			if size > Uint32Size {
+				return ErrFieldSizeTooLarge
+			}
+			bit, err := d.buf.PopUint32(size)
+			if err != nil && err != io.EOF {
+				return err
+			}
+			if typ.Field(i).Name == "_" {
+				continue
+			}
+			st.Field(i).SetUint(uint64(bit))
+		case reflect.Uint64:
+			log.Printf("%s: Uint64", typ.Field(i).Name)
+			if size > Uint64Size {
+				return ErrFieldSizeTooLarge
+			}
+			bit, err := d.buf.PopUint64(size)
+			if err != nil && err != io.EOF {
+				return err
+			}
+			if typ.Field(i).Name == "_" {
+				continue
+			}
+			st.Field(i).SetUint(bit)
+		default:
+			log.Printf("%s: Failed to get type (%s)", typ.Field(i).Name, typ.Kind())
+			// TODO(ymotongpoo): Add exceptional process
+			return nil
+		}
+	}
+	return nil
+}
---
+++ decode_test.go
@@ -0,0 +1,70 @@
+/*
+Copyright 2014 Google Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package bitarray
+
+import (
+	"bytes"
+	"io"
+	"reflect"
+	"testing"
+)
+
+type S struct {
+	F1 uint8  `bit:"1"`
+	_  uint8  `bit:"7"`
+	F2 uint16 `bit:"10"`
+	_  byte
+	_  uint8  `bit:"6"`
+	F3 uint32 `bit:"22"`
+	_  uint16 `bit:"10"`
+	F4 uint64 `bit:"37"`
+}
+
+var data = []byte{
+	0x80, // 1000,0000
+	0xff, // 1111,1111
+	0xc0, // 1100,0000
+	0xff, // 1111,1111
+	0xff, // 1111,1111
+	0xfc, // 1111,1100
+	0x00, // 0000,0000
+	0xff, // 1111,1111
+	0xff, // 1111,1111
+	0xff, // 1111,1111
+	0xff, // 1111,1111
+	0xf8, // 1111,1000
+}
+
+func TestUnmarshal(t *testing.T) {
+	buf := bytes.NewBuffer(data)
+	b := NewBuffer(buf)
+	out := &S{}
+	err := Unmarshal(b, out)
+	if err != nil && err != io.EOF {
+		t.Error(err)
+	}
+
+	want := &S{
+		F1: uint8(0x01),                // 0000,0001
+		F2: uint16(0x03ff),             // 0000,0011|1111,1111
+		F3: uint32(0x003fffff),         // 0000,0000|0011,1111|1111,1111|1111,1111
+		F4: uint64(0x0000001fffffffff), // 0000,0000|0000,0000|0000,0000|0001,1111|1111,1111|1111,1111|1111,1111|1111,1111
+	}
+
+	if !reflect.DeepEqual(want, out) {
+		t.Errorf("want=%#v, out: %#v", want, out)
+	}
+}
