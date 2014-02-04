0173ed312718bdebe02f6bfab98e1059d88967cf
Yoshi Yamaguchi <ymotongpoo AT gmail.com>
2014-02-04 14:48:23 +0900

    implemented support "binary" tag

--- decode_test.go
+++ decode_test.go
@@ -22,33 +22,35 @@ import (
 	"testing"
 )
 
-type S struct {
-	F1 uint8  `bit:"1"`
-	_  uint8  `bit:"7"`
-	F2 uint16 `bit:"10"`
-	_  byte
-	_  uint8  `bit:"6"`
-	F3 uint32 `bit:"22"`
-	_  uint16 `bit:"10"`
-	F4 uint64 `bit:"37"`
-}
+// TestUnmarshal: Case 1) Extract bit array from a byte array.
+// No operation of fetching byte array from arbitrary bit position.
+func TestUnmarshalCase1(t *testing.T) {
+	type S struct {
+		F1 uint8  `bits:"1"`
+		_  uint8  `bits:"7"`
+		F2 uint16 `bits:"10"`
+		_  byte
+		_  uint8  `bits:"6"`
+		F3 uint32 `bits:"22"`
+		_  uint16 `bits:"10"`
+		F4 uint64 `bits:"37"`
+	}
 
-var data = []byte{
-	0x80, // 1000,0000
-	0xff, // 1111,1111
-	0xc0, // 1100,0000
-	0xff, // 1111,1111
-	0xff, // 1111,1111
-	0xfc, // 1111,1100
-	0x00, // 0000,0000
-	0xff, // 1111,1111
-	0xff, // 1111,1111
-	0xff, // 1111,1111
-	0xff, // 1111,1111
-	0xf8, // 1111,1000
-}
+	var data = []byte{
+		0x80, // 1000,0000
+		0xff, // 1111,1111
+		0xc0, // 1100,0000
+		0xff, // 1111,1111
+		0xff, // 1111,1111
+		0xfc, // 1111,1100
+		0x00, // 0000,0000
+		0xff, // 1111,1111
+		0xff, // 1111,1111
+		0xff, // 1111,1111
+		0xff, // 1111,1111
+		0xf8, // 1111,1000
+	}
 
-func TestUnmarshal(t *testing.T) {
 	buf := bytes.NewBuffer(data)
 	b := NewBuffer(buf)
 	out := &S{}
@@ -64,6 +66,48 @@ func TestUnmarshal(t *testing.T) {
 		F4: uint64(0x0000001fffffffff), // 0000,0000|0000,0000|0000,0000|0001,1111|1111,1111|1111,1111|1111,1111|1111,1111
 	}
 
+	if !reflect.DeepEqual(want, out) {
+		t.Errorf("want=%#v, out: %#v", want, out)
+	}
+}
+
+// TestUnmarshal: Case 2) Extract bit array from a byte array.
+// No operation of fetching byte array from arbitrary bit position.
+func TestUnmarsharlCase2(t *testing.T) {
+	type S struct {
+		F1 uint8 `bits:"2"`
+		_  uint8
+		F2 []byte `binary:"12"`
+	}
+
+	var data = []byte{
+		0xd2, // 11|01,0010
+		0x19, // 00|01,1001
+		0x5b, // 01|01,1011
+		0x1b, // 00|01,1011
+		0x1b, // 00|01,1011
+		0xcb, // 11|00,1011
+		0x08, // 00|00,1000
+		0x1d, // 00|01,1101
+		0xdb, // 11|01,1011
+		0xdc, // 11|01,1100
+		0x9b, // 10|01,1011
+		0x19, // 00|01,1001
+		0x3f, // 00|11,1111
+	}
+
+	buf := bytes.NewBuffer(data)
+	b := NewBuffer(buf)
+	out := &S{}
+	err := Unmarshal(b, out)
+	if err != nil && err != io.EOF {
+		t.Error(err)
+	}
+
+	want := &S{
+		F1: uint8(0x03), // 0000,0011
+		F2: []byte("Hello, world"),
+	}
 	if !reflect.DeepEqual(want, out) {
 		t.Errorf("want=%#v, out: %#v", want, out)
 	}
